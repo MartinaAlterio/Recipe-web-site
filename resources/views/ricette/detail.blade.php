933467a8677965613d12b9eba05c08788c434e5e
Martina <martina.alterio93@gmail.com>
2021-06-07 11:42:20 +0200

    inizio css

--- resources/views/ricette/detail.blade.php
+++ resources/views/ricette/detail.blade.php
@@ -1,14 +1,16 @@
 @extends('template')
 
 @section('content')
-    <h1>{{$recipe->name}}</h1>
-    <p>Sottotitolo: {{$recipe->subheading}}</p>
+    <div class="titleContainer" style="background-image: url('{{asset('storage/images/recipes/'.$recipe->image)}}')">
+       <h1 class="title">{{$recipe->name}}</h1>
+       <p>Sottotitolo: {{$recipe->subheading}}</p>
+    </div>
     <p>Descrizione: {{$recipe->description}}</p>
     <div> Ingredienti:
         <ul>
             @foreach($recipe->ingredients as $ingredient)
                 @if($ingredient->active === 1)
-                    <li><a href="/ingredienti/{{$ingredient->url}}">{{$ingredient->name}}</a></li>
+                    <li><a href="/ingredienti/{{$ingredient->url}}" class="button">{{$ingredient->name}}</a></li>
                 @else
                     <li>{{$ingredient->name}}</li>
                 @endif
