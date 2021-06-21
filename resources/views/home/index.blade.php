29f841098b5d8cbde08bf0d2efea45b7a18cdea1
Martina <martina.alterio93@gmail.com>
2021-06-21 17:47:38 +0200

    prove generali stilizzazione home e pagina elenco ricette

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,33 +1,36 @@
-@extends('template')
+<div class="home">
+    @extends('template')
 
-@section('content')
-    <div class="pageContainer">
-        <div class="titleContainer" style="background-image: url('{{asset('storage/images/recipes/'.$home->title->image)}}')"></div>
-        <div class="subject">
-            {{$home->title->content}}
+    @section('content')
+        <div class="pageContainer home_page" >
+            <div class="titleContainer home_title" style="background-image: url('{{asset('storage/images/recipes/'.$home->title->image)}}')"></div>
+            <div class="subject home_subject">
+                {{$home->title->content}}
+            </div>
+            <div class="border home_border"></div>
+            <div class="textContainer">
+                <div>
+                    <div>{{$home->recipes->content}}</div>
+                    <div>{{$home->recipes->image}}</div>
+                </div>
+                <div>
+                    <div>{{$home->ingredients->content}}</div>
+                    <div>{{$home->ingredients->image}}</div>
+                </div>
+                <div>
+                    <div>{{$home->about_me->content}}</div>
+                    <div>{{$home->about_me->image}}</div>
+                </div>
+            </div>
         </div>
-        <div class="border"></div>
-        <div>
-            <div>
-                <div>{{$home->recipes->content}}</div>
-                <div>{{$home->recipes->image}}</div>
-            </div>
-            <div>
-                <div>{{$home->ingredients->content}}</div>
-                <div>{{$home->ingredients->image}}</div>
-            </div>
-            <div>
-                <div>{{$home->about_me->content}}</div>
-                <div>{{$home->about_me->image}}</div>
-            </div>
-        </div>
-    </div>
 
 
-@endsection
+    @endsection
+</div>
 
-//bisogna rivedere l'idea immagine+testo per la schermata home.Creazione di serie di immagini che si susseguono dinamicamente.
-//voglio modificare l'header nella home?
-//voglio splittare la scritta tra top e bot dell'immagine?
-//voglio una sola immagine?
-//voglio la home altrettanto pulita?
+
+<!--bisogna rivedere l'idea immagine+testo per la schermata home.Creazione di serie di immagini che si susseguono dinamicamente.
+voglio modificare l'header nella home?
+voglio splittare la scritta tra top e bot dell'immagine?
+voglio una sola immagine?
+voglio la home altrettanto pulita?-->
--- resources/views/ricette/macro.blade.php
+++ resources/views/ricette/macro.blade.php
@@ -1,28 +1,34 @@
 @extends('template')
 
 @section('content')
-    <div> foto ad effetto
-         <h1>Qui trovi l'elenco completo delle mie ricette!</h1> <br>
+    <div class="pageContainer">
+        <div class="textContainer macroText">
+            @if(isset($macros))
+                <ul class="macroContainer">
+                    @foreach($macros as $value)
+                        <div class="macro">
+                            <div class="text_macro">
+                                <li class="subject">{{$value->name}}</li>
+                                <div class="border"></div>
+                                <div class="list_macro">
+                                    @foreach($value->categories as $value)
+                                        <li><a href="/ricette/{{$value->url}}">{{$value->name}}</a></li>
+                                        <div class="list_recipes">
+                                            @foreach($value->recipes as $recipe)
+                                                <li><a href="/ricette/{{$value->url}}/{{$recipe->url}}">{{$recipe->name}}</a></li>
+                                            @endforeach
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div class="image_macro"> </div>
+                        </div>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
     </div>
 
-        @if(isset($macros))
-            <ul>
-                @foreach($macros as $value)
-
-                    <li>{{$value->name}}</li>
-                    @foreach($value->categories as $value)
-                        <li><a href="/ricette/{{$value->url}}">{{$value->name}}</a></li>
-                        @foreach($value->recipes as $recipe)
-                               <li><a href="/ricette/{{$value->url}}/{{$recipe->url}}">{{$recipe->name}}</a></li>
-                        @endforeach
-                    @endforeach
-
-                @endforeach
-
-            </ul>
-
-
-        @endif
 @endsection
 
 
