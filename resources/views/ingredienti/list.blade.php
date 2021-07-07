090551eff88b71aee4efc3aaa7f234fa2d4964ba
Martina <martina.alterio93@gmail.com>
2021-07-07 22:52:22 +0200

    inizio stilizzazione pagina ingredienti

--- resources/views/ingredienti/list.blade.php
+++ resources/views/ingredienti/list.blade.php
@@ -1,17 +1,16 @@
 @extends('template')
 
 @section('content')
-    <div class="pageContainer listIngredients">
-        <div>
-            <p> </p>
-        </div>
-        <div class="textContainer">
+    <div class="main_container main_container--ingredients">
+        <div class="main_image main_image--ingredients" style="background-image: url('{{asset('storage/images/ingredients/farine.jpg')}}')"></div>
+        <div class="main_title main_title1">INGREDIENTI</div>
+        <div class="main_image main_image--ingredients" style="background-image: url('{{asset('storage/images/ingredients/zuccheri.jpeg')}}')"></div>
+    <div class="page_container">
             @foreach($list as $ingredient)
-                <div class="text_image">
-                    <div class="subject_reverse"><a href="/ingredienti/{{$ingredient->url}}">{{$ingredient->name}}</a></div>
-                    <div class="border_reverse"></div>
-                    <div class="image" style="background-image: url('{{asset('storage/images/ingredients/'.$ingredient->image)}}')"> </div>
-                    <div class="text"> </div>
+                <div class="main_content">
+                    <div class="title"><a href="/ingredienti/{{$ingredient->url}}">{{$ingredient->name}}</a></div>
+                    <div class="main_content__image" style="background-image: url('{{asset('storage/images/ingredients/'.$ingredient->image)}}')"> </div>
+                    <div class="main_content__text"> </div>
                 </div>
             @endforeach
         </div>
