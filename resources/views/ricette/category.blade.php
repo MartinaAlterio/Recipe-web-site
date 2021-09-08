e5b1bc7a18d508110afba2540e17df46f069ed0b
Martina <martina.alterio93@gmail.com>
2021-09-08 19:32:56 +0200

    lavorato al responsive della pagina categorie

--- resources/views/ricette/category.blade.php
+++ resources/views/ricette/category.blade.php
@@ -5,19 +5,22 @@
         @if($category === null)
             <div class="title" >Al momento non è possibile mostrare la pagina di dettaglio della categoria {{$url_category}}</div>
         @else
-            <div class="main_title main_title--category">{{$category->name}}</div>
-            <div class="border border--category"></div>
-            <div class="main_image" style="background-image: url('{{asset('storage/images/recipes/category/'.$category->image)}}')"></div>
+
+            <div class="main_image main_image--category" style="background-image: url('{{asset('storage/images/recipes/category/'.$category->image)}}')">
+                <div class="cover_image">
+                    <div class="main_title main_title--category">{{$category->name}}</div>
+                </div>
+            </div>
             <div class="main_text"> {{$category->description}}</div>
             <div class="page_container page_container--category">
                 <ul class="container_list container_list--category">
                     @foreach($category->recipes as $recipe)
-                        <div class="list_element list_element--category">
-                            <a href="/ricette/{{$category->url}}/{{$recipe->url}}">
+                        <a href="/ricette/{{$category->url}}/{{$recipe->url}}">
+                            <div class="list_element list_element--category">
                                 <div class="list__image list__image--category" style="background-image: url('{{asset('storage/images/recipes/detail/'.$recipe->image)}}')"></div>
                                 <div class="list__title list__title--category">{{strtoupper($recipe->name)}}</div>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                     @endforeach
                 </ul>
             </div>
