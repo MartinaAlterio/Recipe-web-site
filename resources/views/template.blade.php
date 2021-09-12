049a7f3d44494e39d8f5053822aef247e7e2638a
Martina <martina.alterio93@gmail.com>
2021-09-12 17:15:44 +0200

    aggiunto file js e continuato data entry

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -45,27 +45,6 @@
             </div>
         </div>
     </div>
-    <script>
-        let header = document.querySelector('.header_home');
-        if(header != null) {
-            let over = false;
-            window.addEventListener('scroll', () => {
-                if(over === false && window.scrollY > header.scrollHeight ) {
-                    header.classList.add("over_header");
-                    over = true;
-                } else if (over === true && window.scrollY < header.scrollHeight) {
-                    header.classList.remove("over_header");
-                    over = false;
-                }
-            })
-        }
-
-        let flash_messages = document.querySelectorAll('.flash_message');
-        flash_messages.forEach((flash_message)=> {
-            flash_message.addEventListener('click', ()=> {
-                flash_message.classList.add('closed');
-            })
-        })
-    </script>
+    <script src="{{asset('/js/main.js')}}"></script>
 </body>
 </html>
